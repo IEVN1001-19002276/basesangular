6d2e2d195d3525c100ba0490d46e42e5aa7252af
Victor Daniel Bueno Rocha <capsaitama@gmail.com>
2024-10-09 20:50:43 -0600

    Correccion

--- basesAngular/src/app/product/product-list/product-list.component.ts
+++ basesAngular/src/app/product/product-list/product-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { IProductos } from '../productos';
  
 @Component({
     selector: 'app-product-list',
@@ -8,42 +9,45 @@ import { Component } from '@angular/core';
  
 export class ProductListComponent {
  
-  imageWidth: number = 50;
-  imageMargin: number = 2;
-  muestraImagenes: boolean = true;
- 
-    productos: any[] = [
+    imageWidth: number = 50;
+    imageMargin: number = 2;
+    muestraImagen: boolean = false;
+    listFilter: string = '';
+    mostrarImagenes(): void {
+      this.muestraImagen = !this.muestraImagen;
+    }
+    productos: IProductos[] = [
         {
-            "productoId": 1,
-            "modelo": "sentra",
-            "descripcion": "5 puertas",
-            "year": "marzo 12 2021",
-            "precio": 120000,
-            "marca": "nisan",
-            "color": "blanco",
-            "imgUrl": "https://i.pinimg.com/originals/c4/dd/8c/c4dd8c121ef1da932ac9ad463554fe4a.jpg"
+            "ProductoID": 1,
+            "Modelo": "sentra",
+            "Descripcion": "5 puertas",
+            "Year": "marzo 12 2021",
+            "Precio": 120000,
+            "Marca": "nisan",
+            "Color": "blanco",
+            "ImageURL": "https://i.pinimg.com/originals/c4/dd/8c/c4dd8c121ef1da932ac9ad463554fe4a.jpg"
        
         },
         {
-            "productoId": 2,
-            "modelo": "a4",
-            "descripcion": "4 puertas",
-            "year": "marzo 12 2021",
-            "precio": 130000,
-            "marca": "volsvagen",
-            "color": "negro",
-            "imgUrl": "https://somoskudasai.com/wp-content/uploads/2020/08/EgocUdbU4AABJgL.jpg"
+            "ProductoID": 2,
+            "Modelo": "a4",
+            "Descripcion": "4 puertas",
+            "Year": "marzo 12 2021",
+            "Precio": 130000,
+            "Marca": "volsvagen",
+            "Color": "negro",
+            "ImageURL": "https://somoskudasai.com/wp-content/uploads/2020/08/EgocUdbU4AABJgL.jpg"
        
         },
         {
-            "productoId": 3,
-            "modelo": "a5",
-            "descripcion": "2 puertas",
-            "year": "marzo 12 2021",
-            "precio": 150000,
-            "marca": "chevrolet",
-            "color": "rojo",
-            "imgUrl": "https://img.remediosdigitales.com/a301c8/itasha-ferrari-430/1366_2000.jpg"
+            "ProductoID": 3,
+            "Modelo": "a5",
+            "Descripcion": "2 puertas",
+            "Year": "marzo 12 2021",
+            "Precio": 150000,
+            "Marca": "chevrolet",
+            "Color": "rojo",
+            "ImageURL": "https://img.remediosdigitales.com/a301c8/itasha-ferrari-430/1366_2000.jpg"
        
         },  
     ]
---
+++ basesAngular/src/app/product/producto-fi.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { ProductoFiPipe } from './producto-fi.pipe';
+
+describe('ProductoFiPipe', () => {
+  it('create an instance', () => {
+    const pipe = new ProductoFiPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
